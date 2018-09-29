a3db742d6030f0c270206685335b6fda7bd4b3f2
David Bugsracer Cantú <david@bugsracer.com>
2018-09-29 17:13:06 -0500

    Initial Commit

---
+++ MoviesCustomRows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesCustomRows
+//
+//  Created by David A Cantú Delgado on 9/29/18.
+//  Copyright © 2018 Bugsracer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
